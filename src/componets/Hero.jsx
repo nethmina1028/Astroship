25052423819b366a17e32a57d53349baf00a39f7
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-08-29 19:43:47 +0530

    #3

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import marketing from '../assets/marketing-strategy.png';
 const Hero = () => {
   return (
-    <div className='md:px-[5rem]'>Hero</div>
+    <div className='md:px-[5rem]'>
+        <div className='flex flex-wrap-reverse items-center justify-between py-[5rem]'>
+
+        
+        <div>
+
+                 <h1>Building digital products & brands.</h1>
+                 <p>Here at flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        
+            <div>
+             <button className='text-base font-sans bg-accent text-white h-[2.6015rem] w-[7.0325rem] rounded-[0.615rem] hover:'>Start free trial</button>
+             <button className=' border-gray border-2 text-base font-sans bg-white text-black h-[2.6015rem] w-[7.0325rem] rounded-[0.615rem] hover:'>Pricing & FAQ </button>
+            </div>
+
+        </div>
+
+        <div>
+         <img src={marketing}  alt="Logo" />
+        </div>
+
+     </div>
+    </div>
   )
 }
 
