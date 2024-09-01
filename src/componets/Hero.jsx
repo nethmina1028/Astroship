983f0a2f67d7d51db3684017a22b062114c41c54
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-09-01 18:21:03 +0530

    #3

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -5,13 +5,15 @@ const Hero = () => {
         <div className='flex flex-wrap-reverse items-center justify-between py-[5rem]'>
 
         
-        <div>
-
-                 <h1>Building digital products & brands.</h1>
-                 <p>Here at flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <div className=''>
+               <div className=''>
+               <h1 className='font-extrabold text-[4.1231rem] '>Building digital products & brands.</h1>
+               <p className='text-gray text-[1.1734rem]'>Here at flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+               </div>
+                
         
             <div>
-             <button className='text-base font-sans bg-accent text-white h-[2.6015rem] w-[7.0325rem] rounded-[0.615rem] hover:'>Start free trial</button>
+             <button className='  text-base font-sans bg-accent text-white h-[2.6015rem] w-[7.0325rem] rounded-[0.615rem] hover:'>Start free trial</button>
              <button className=' border-gray border-2 text-base font-sans bg-white text-black h-[2.6015rem] w-[7.0325rem] rounded-[0.615rem] hover:'>Pricing & FAQ </button>
             </div>
 
